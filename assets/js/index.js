05b44c6d9d1ac2738becae246ac3ad5a27987fcb
kurocraft0312 <kurocraft0312@gmail.com>
2020-01-11 19:58:26 +0900

    Slickを消しました。

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,29 +1,2 @@
 'use strict';
 
-/**************************************************
-slick
-**************************************************/
-$(document).ready(function() {
-    $('.slider').slick({
-        // setting-name:setting-value
-        autoplay: true,
-        adaptiveHeight: true,
-        arrows: false,
-        dots: true,
-        infinite: true,
-        mobileFirst: true,
-        responsive: [
-            {
-                breakpoint: 767,
-                setting: {
-                    slidesToShow: 3,
-                    slidesToScroll: 3,
-                    infinite: true,
-                    dots: true,
-                }
-            },
-        ],
-        slidesToShow: 1,
-        slidesToScroll: 1,
-    });
-});
