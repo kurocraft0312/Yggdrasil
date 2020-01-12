48331193ca8d5f4c27606608e159b7e680f8159e
kurocraft0312 <kurocraft0312@gmail.com>
2020-01-12 19:00:11 +0900

    コード微調整。

--- components/ogp.php
+++ components/ogp.php
@@ -22,23 +22,23 @@ function yggdrasil_meta_ogp() {
             $ogp_desc = get_bloginfo('description');
             $ogp_url = home_url();
         }
-        $ogp_type = (is_front_page() || is_home() ) ? 'website' : 'article';
         if ( is_singular() && has_post_thumbnail() ) {
             $ps_thumb = wp_get_attachment_image_src( get_post_thumbnail_id(), 'full' );
             $ogp_img = $ps_thumb[0];
         } else {
             $ogp_img = 'TOP画像がないときに設定する画像';
         }
+        $ogp_type = (is_front_page() || is_home() ) ? 'website' : 'article';
         
         $insert .= '<meta property="og:title" content="'.esc_attr($ogp_title).'" />' . "\n";
         $insert .= '<meta property="og:description" content="'.esc_attr($ogp_descr).'" />' . "\n";
-        $insert .= '<meta property="og:type" content="'.$ogp_type.'" />' . "\n";
         $insert .= '<meta property="og:url" content="'.esc_url($ogp_url).'" />' . "\n";
         $insert .= '<meta property="og:image" content="'.esc_url($ogp_img).'" />' . "\n";
+        $insert .= '<meta property="og:type" content="'.$ogp_type.'" />' . "\n";
         $insert .= '<meta property="og:site_name" content="'.esc_attr(get_bloginfo('name')).'" />' . "\n";
+        $insert .= '<meta property="og:locale" content="ja_JP" />' . "\n";
         $insert .= '<meta name="twitter:card" content="summary_large_image" />' . "\n";
         $insert .= '<meta name="twitter:site" content="@kurocraft7522" />' . "\n";
-        $insert .= '<meta property="og:locale" content="ja_JP" />' . "\n";
         $insert .= '<meta property="fb:app_id" content="ここにappIDを入力">' . "\n";
     
         echo $insert;
